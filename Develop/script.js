37ca0fe98e0c03f944d5ac0f7720592bea086a31
SpookyAura7 <handscrews@outlook.com>
2022-05-21 14:35:37 -0500

    putting parameters together with notifications

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,20 @@
 // Assignment code here
+var characterLength = window.prompt("How many characters would you like?");
+  if (characterLength>= 8 && characterLength<= 128) {
+    console.log(characterLength);
+    window.alert("You have selected " + characterLength + "!");
+  } else if (characterLength< 8 || characterLength> 128) {
+    window.alert("Try Again!");
+  }
 
+var uppercase = window.confirm("Would you like to include UPPERCASE letters?");
+  console.log(uppercase);
+  if (uppercase) {
+    window.alert("It appears you desire uppercase letters in your password.");
+  } else {
+    window.alert("It appears you loathe uppercase letters. You have been relegated to lowercase letters.");
+  }
+  
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
