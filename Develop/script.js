f6e602d1efe96b9d9c11c8e1cf6ea57b8020b67c
SpookyAura7 <handscrews@outlook.com>
2022-05-21 15:03:17 -0500

    password parameters set, with confirmation messages

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,22 @@
 // Assignment code here
-var characterLength = window.prompt("How many characters would you like?");
-  if (characterLength>= 8 && characterLength<= 128) {
-    console.log(characterLength);
-    window.alert("You have selected " + characterLength + "!");
-  } else if (characterLength< 8 || characterLength> 128) {
-    window.alert("Try Again!");
+function characterPrompt() {  
+  var characterLength = window.prompt("How many characters would you like?");
+    if (characterLength>= 8 && characterLength<= 128) {
+      console.log(characterLength);
+      window.alert("You have selected " + characterLength + "!");
+    } else if (characterLength< 8 || characterLength> 128) {
+      window.alert("Try Again!");
+      characterPrompt();
+    } else {
+      window.alert("Alright smarty pants, that wasn't even a number.");
+      characterPrompt();
+    }
   }
+characterPrompt();
 
-var uppercase = window.confirm("Would you like to include UPPERCASE letters?");
+
+
+var uppercase = window.confirm("Would you like to include UPPERCASE letters in your password?");
   console.log(uppercase);
   if (uppercase) {
     window.alert("It appears you desire uppercase letters in your password.");
@@ -15,6 +24,21 @@ var uppercase = window.confirm("Would you like to include UPPERCASE letters?");
     window.alert("It appears you loathe uppercase letters. You have been relegated to lowercase letters.");
   }
   
+var numbers = window.confirm("Would you like to include numbers in your password?");
+  console.log(numbers);
+  if (numbers) {
+    window.alert("It appears you desire numbers in your password.");
+  } else {
+    window.alert("It appears you are too cool for numbers and they will be ommitted from your password.");
+  }
+
+var specialChar = window.confirm("Would you like characters of the special variety in your password? (i.e. @ # $ %)");
+  console.log(specialChar)
+  if (specialChar) {
+    window.alert("It appears you would enjoy the most special of characters in your super-secure password.");
+  } else {
+    window.alert("It appears the characters aren't so special after all. They will be ommitted from your password.");
+  }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
