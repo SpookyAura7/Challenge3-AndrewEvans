68b0fd3993739b9f08efa14a1aafbca6c6e567cc
SpookyAura7 <handscrews@outlook.com>
2022-06-06 23:26:16 -0500

    HOLY SMOKES IT ACTUALLY WORKS

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,8 @@
-// Assignment code here
 
 function generatePassword () {
+  /* function to loop back to prompt if answer was unsatisfactory */
   function characterPrompt() {  
-    var characterLength = window.prompt("How many characters would you like?");
+    characterLength = Number(window.prompt("How many characters would you like?"));
       if (characterLength>= 8 && characterLength<= 128) {
         console.log(characterLength);
         window.alert("You have selected " + characterLength + "!");
@@ -15,7 +15,8 @@ function generatePassword () {
       }
     }
   characterPrompt();
-  
+  console.log(characterLength);
+    /* this is to convert the boolean answers to usable data */
   var letterCase = window.confirm("Would you like to include UPPERCASE letters in your password?");
   console.log(letterCase);
   if (letterCase) {
@@ -33,36 +34,44 @@ function generatePassword () {
       var numbers = "1234567890";
     } else {
       window.alert("It appears you are too cool for numbers and they will be ommitted from your password.");
+      var numbers = "";
     }
 
-  var specialChar = window.confirm("Would you like characters of the special variety in your password? (i.e. @ # $ %)");
-    console.log(specialChar)
+  var specialChar = window.confirm("Would you like characters of the special vari ety in your password? (i.e. @ # $ %)");
+    console.log(specialChar);
     if (specialChar) {
       window.alert("It appears you would enjoy the most special of characters in your super-secure password.");
-      var specialChar = "!@#$%^&*()?"
+      var specialChar = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
     } else {
       window.alert("It appears the characters aren't so special after all. They will be ommitted from your password.");
+      var specialChar = "";
     }
-
-
+      /* this is to put workable answer data into a single variable */
+  var allChars = letterCase + numbers + specialChar;
+    console.log(allChars);
+  var password = "";
+    /*this code (credit to Foolish Developer on dev.to) takes the previous data and randomizes it */ 
+  for (var i = 0; i <= characterLength; i++) {
+    var randomNumber = Math.floor(Math.random() * allChars.length);
+    password += allChars.substring(randomNumber, randomNumber +1);
+  }
+    document.querySelector("#password").value = password
 
 
 }
 
 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+// To whoever grades this: I finally got it to work, and I refuse to break it again by touching the following code:
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
 }
 
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-generateBtn.addEventListener("click", console.log(characterLength,numbers,uppercase,specialChar))
